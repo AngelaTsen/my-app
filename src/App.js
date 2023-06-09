8a5c2335dfa0d8de849698a1d874fbcd6dd0a5f7
AngelaTsen <125123519+AngelaTsen@users.noreply.github.com>
2023-06-09 17:33:37 +0300

    add header

--- src/App.js
+++ src/App.js
@@ -3,19 +3,15 @@ import {UserForm} from "./components/UserForm/UserForm";
 import { Header } from "./components/Header/Header";
 import { Banner } from "./components/Banner/Banner";
 import { Blog } from "./components/Blog/Blog";
-import { DataTable } from "./components/DataTable/DataTable";
-import {router} from "./pages/router.config.js"
 
 
 const App = () => {
   return (
     <>
-      {/* <Header class="header"></Header>
+      <Header class="header"></Header>
       <Banner></Banner>
       <UserForm></UserForm>
-      <Blog></Blog> */}
-        {/* <DataTable></DataTable> */}
-<RouterProvaider router = {router} />
+      <Blog></Blog>
     
     </>
   );
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,11 +4,11 @@ export const Header = ({children}) => <header className="header">{children}
 <div className="header__container">
           <div className="left-block"></div>
           <div className="right-blocks">
-            <div className="right-blocks__icon"></div>
-            <div className="right-blocks__icon"></div>
-            <div className="right-blocks__icon"></div>
-            <div className="right-blocks__icon"></div>
-            <div className="right-blocks__icon"></div>
+            <div className="right-blocks__icon">Breakfast</div>
+            <div className="right-blocks__icon">Starter</div>
+            <div className="right-blocks__icon">Seafood</div>
+            <div className="right-blocks__icon">Vegan</div>
+            <div className="right-blocks__icon">Dessert</div>
           </div>
         </div>
 
