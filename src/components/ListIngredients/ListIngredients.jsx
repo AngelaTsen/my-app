71b74cb71bbf3c5a47cd74ff86d1edd0cfcb891e
AngelaTsen <125123519+AngelaTsen@users.noreply.github.com>
2023-06-18 17:08:57 +0300

    Add popup in list

--- src/components/ListIngredients/ListIngredients.jsx
+++ src/components/ListIngredients/ListIngredients.jsx
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
-import "./ListIngredients.scss"
+import "./ListIngredients.scss";
 
 export const ListIngredients = ({ idMeal }) => {
   const [ingredients, setIngredients] = useState([]);
+  const [showPopup, setShowPopup] = useState(false); // Доданий стан для попапу
 
   useEffect(() => {
     fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`)
@@ -24,20 +25,38 @@ export const ListIngredients = ({ idMeal }) => {
       .catch((error) => console.error("An error occurred:", error));
   }, [idMeal]);
 
+  const handlePopupToggle = () => {
+    setShowPopup(!showPopup);
+  };
+
   return (
     <div className='list'>
       <h2 className='list__h2'>Ingredients:</h2>
+      <p className='list__p'>Check what you have:</p>
       {ingredients.map((ingredient, index) => (
-         <div key={index} className='list__round'>
-         
+        <div key={index} className='list__round'>
           <input className='list__inp' type="checkbox" id={`ingredient-${index}`} />
           <label className='list__lab' htmlFor={`ingredient-${index}`}>
-          {ingredient.ingredient} - <b>{ingredient.measure}</b>
+            {ingredient.ingredient} - <b>{ingredient.measure}</b>
           </label>
-          </div>
-      
+        </div>
       ))}
+      <button className="popup-button" onClick={handlePopupToggle}>Show list</button>
+
+      {showPopup && (
+        <div className="popup">
+          <h3 className='popup-h3'>You need to buy:</h3>
+          <ul>
+            {ingredients
+              .filter((ingredient, index) => !document.getElementById(`ingredient-${index}`).checked)
+              .map((ingredient, index) => (
+                <li key={index} className="popup-li">
+                  {ingredient.ingredient} - {ingredient.measure}
+                </li>
+              ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
 };
-export default ListIngredients;
